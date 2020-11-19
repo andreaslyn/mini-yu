fbe8c4da68251f46c02dee978257a4182ece0c81
Andreas Lynge <andreaslyn@gmail.com>
2020-11-19 21:51:30 +0100

    Cleanup examples

---
+++ examples/check-all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+args="$@"
+
+dir=`dirname "$0"`
+dir=`realpath "$dir"`
+
+cd "$dir"
+
+tests=`ls *.yu`
+
+mini_yu="../yuc"
+
+run_test() {
+  echo "CHECK $1"
+  "$mini_yu" "$1" $args
+}
+
+for t in $tests; do
+  run_test "$t"
+done
