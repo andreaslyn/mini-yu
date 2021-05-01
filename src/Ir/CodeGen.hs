andreaslyn/mini-yu

commit 12b0a33b7e21b75e2dd94c52d9c25971c18de0a1

Fix tail recursive variable call performance

src/Ir/CodeGen.hs
@@ -15,7 +15,7 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans (lift)
 import Control.Monad (when)
 import qualified Data.List as List
-import Data.Maybe (isJust)
+import Data.Maybe (isNothing, isJust)
 
 import Control.Exception (assert)
 --import Debug.Trace (trace)
@@ -250,6 +250,9 @@ writePapFunClosure c v rs n = do
   writeStr ") {"
   incIndent >> newLine
   writeTempsAndIncs recIdx 0
+  if (isNothing v)
+  then writeStr "yur_unref(self);" >> newLine
+  else return ()
   writeStr "return "
   writeStr (funName c)
   writeStr "("

src/Ir/RefCountIr.hs
@@ -362,10 +362,10 @@ irResetFunExpr v0 n0 (PreVarFieldCnt v n e _) =
   else irResetFunExpr v0 n0 e
 
 isVarArgIn :: Var -> PreLetExpr -> Bool
-isVarArgIn v (PreAp _ _ rs) = isVarArgInRefs v rs
+isVarArgIn v (PreAp _ r rs) = isVarArgInRefs v (r : rs)
 isVarArgIn v (PrePap _ rs) = isVarArgInRefs v rs
 isVarArgIn _ (PreMkLazy _ _ _) = False
-isVarArgIn v (PreForce _ _ rs) = isVarArgInRefs v (preForceProjArgs rs)
+isVarArgIn v (PreForce _ r rs) = isVarArgInRefs v (r : preForceProjArgs rs)
 isVarArgIn v (PreCtorBox _ rs) = isVarArgInRefs v rs
 isVarArgIn _ (PreProj _ _) = False
 isVarArgIn _ (PreReset _) = False
@@ -463,12 +463,12 @@ irIncDecFunExpr ctx (PrePforce pc v rs e2 lli) =
       ctx'' = IntSet.difference ctx' s
       e2' = irIncDecFunExpr (IntSet.insert v ctx'') e2
   in prefix1 (prefix2 (PrePforce pc v rs e2' lli))
-irIncDecFunExpr ctx (PreLet v e1@(PreAp _ _ rs) e2 lli) =
-  irIncDecLetExprAp ctx v e1 e2 rs lli
+irIncDecFunExpr ctx (PreLet v e1@(PreAp _ r rs) e2 lli) =
+  irIncDecLetExprAp ctx v e1 e2 (r : rs) lli
 irIncDecFunExpr ctx (PreLet v e1@(PrePap _ rs) e2 lli) =
   irIncDecLetExprAp ctx v e1 e2 rs lli
-irIncDecFunExpr ctx (PreLet v e1@(PreForce _ _ rs) e2 lli) =
-  irIncDecLetExprAp ctx v e1 e2 (preForceProjArgs rs) lli
+irIncDecFunExpr ctx (PreLet v e1@(PreForce _ r rs) e2 lli) =
+  irIncDecLetExprAp ctx v e1 e2 (r : preForceProjArgs rs) lli
 irIncDecFunExpr ctx (PreLet v e1@(PreCtorBox _ []) e2 lli) =
   let (prefix, ctx') = decDeadVars ctx lli
       e2' = irIncDecFunExpr (IntSet.insert v ctx') e2

commit 2089b4c509db8ea2dc3768f568212b161dda5277

Fix memory leak introduced by 12b0a33b7e21b75e2dd94c52d9c25971c18de0a1

src/Ir/CodeGen.hs
@@ -250,7 +250,7 @@ writePapFunClosure c v rs n = do
   writeStr ") {"
   incIndent >> newLine
   writeTempsAndIncs recIdx 0
-  if (isNothing v)
+  if isNothing v
   then writeStr "yur_unref(self);" >> newLine
   else return ()
   writeStr "return "

src/Ir/RefCountIr.hs
@@ -365,7 +365,7 @@ isVarArgIn :: Var -> PreLetExpr -> Bool
 isVarArgIn v (PreAp _ r rs) = isVarArgInRefs v (r : rs)
 isVarArgIn v (PrePap _ rs) = isVarArgInRefs v rs
 isVarArgIn _ (PreMkLazy _ _ _) = False
-isVarArgIn v (PreForce _ r rs) = isVarArgInRefs v (r : preForceProjArgs rs)
+isVarArgIn v (PreForce _ _ rs) = isVarArgInRefs v (preForceProjArgs rs)
 isVarArgIn v (PreCtorBox _ rs) = isVarArgInRefs v rs
 isVarArgIn _ (PreProj _ _) = False
 isVarArgIn _ (PreReset _) = False
@@ -467,8 +467,8 @@ irIncDecFunExpr ctx (PreLet v e1@(PreAp _ r rs) e2 lli) =
   irIncDecLetExprAp ctx v e1 e2 (r : rs) lli
 irIncDecFunExpr ctx (PreLet v e1@(PrePap _ rs) e2 lli) =
   irIncDecLetExprAp ctx v e1 e2 rs lli
-irIncDecFunExpr ctx (PreLet v e1@(PreForce _ r rs) e2 lli) =
-  irIncDecLetExprAp ctx v e1 e2 (r : preForceProjArgs rs) lli
+irIncDecFunExpr ctx (PreLet v e1@(PreForce _ _ rs) e2 lli) =
+  irIncDecLetExprAp ctx v e1 e2 (preForceProjArgs rs) lli
 irIncDecFunExpr ctx (PreLet v e1@(PreCtorBox _ []) e2 lli) =
   let (prefix, ctx') = decDeadVars ctx lli
       e2' = irIncDecFunExpr (IntSet.insert v ctx') e2